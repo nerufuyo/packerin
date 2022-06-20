ea5a2847eceba1f67f8c35e8e16d5d270af9f2b1
unknown <ojanakmal800@gmail.com>
2022-06-20 22:44:37 +0700

    back end integration firestore data

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main()
 async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
