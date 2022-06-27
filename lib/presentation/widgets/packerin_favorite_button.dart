0411df4c152add1eab963beffbeda23ea6e7ebb4
abimmayu <sakilavino@gmail.com>
2022-06-27 09:20:36 +0700

    try to make a favorite

---
+++ lib/presentation/widgets/packerin_favorite_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(
+          () {
+            isFavorite = !isFavorite;
+          },
+        );
+      },
+    );
+  }
+}
